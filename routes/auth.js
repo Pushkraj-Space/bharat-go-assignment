3b67ae56b5d1c83bd6125beecfd4d14d1fbc4ddc
pushkraj <dev.pushkraj@gmail.com>
2023-07-24 13:52:08 +0530

    success

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
-
 const authController = require('../controllers/authController');
 
+// *POST /auth/register*: Register a new user.
 router.post('/register', authController.newUser);
 
+// *POST /auth/login*: Authenticate and generate a JWT token for the user.
 router.post('/login', authController.loginUser);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,20 +2,21 @@ const router = require('express').Router();
 const authToken = require('../authToken');
 const userController = require('../controllers/userController');
 
-// 3. *GET /users*: Retrieve a list of all users.
+// *GET /users*: Retrieve a list of all users.
 router.get('/', authToken, userController.getAllUsers);
 
-// 4. *GET /users/:id*: Retrieve a specific user by their ID.
+// *GET /users/:id*: Retrieve a specific user by their ID.
 router.get('/:id', authToken, userController.getUserById);
 
-// 5. *POST /users*: Create a new user.
+// *POST /users*: Create a new user.
 router.post('/', authToken, userController.newUser);
 
-// 6. *PUT /users/:id*: Update an existing user by their ID.
+// *PUT /users/:id*: Update an existing user by their ID.
 router.put('/:id', authToken, userController.updateUser);
 
-// 7. *DELETE /users/:id*: Delete a user by their ID.
+// *DELETE /users/:id*: Delete a user by their ID.
 router.delete('/:id', authToken, userController.deleteUser);
+
 module.exports = router;
 
 
